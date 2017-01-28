612ba7fb16283c6cda9e796f0c89ca230acedc01
Aileen Rowan <aileen.s.rowan@gmail.com>
2017-01-27 20:16:06 -0500

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :grids, only: [:show, :create, :update]
 
-  root to: "welcome#index"
+  root to: "grids#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
